ed8ed2b0574cfd7b4c25afbd0799a66462efff79
Samuel-UC <68044712+Samuel-UC@users.noreply.github.com>
2021-01-05 22:17:29 -0800

    basic element

--- accordion.js
+++ accordion.js
@@ -1,1 +1,16 @@
+class Accordion extends HTMLElement {
+    constructor() {
 
+        // Always call super first in constructor
+        super();
+    }
+
+    connectedCallback() {
+
+        let button = document.createElement('button');
+
+        
+    }
+}
+
+customElements.define('accordion-parent', Accordion);

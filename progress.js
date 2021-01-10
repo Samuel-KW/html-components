df6c60deba082408a3ee5279e736b5612524a773
Samuel-UC <68044712+Samuel-UC@users.noreply.github.com>
2021-01-09 16:18:18 -0800

    better style handling

--- progress.js
+++ progress.js
@@ -1,61 +1,79 @@
-class Progress extends HTMLElement {
-    constructor() {
-
-        // Always call super first in constructor
-        super();
-
-        this.parent = document.createElement('div');
-        this.child = document.createElement('div');        
-    }
-
-    connectedCallback() {
-
-        this.parent.style = 'margin-top: 5px; background-color: #eee; height: 25px; min-width: 100px; width: auto;';
-        this.child.style = 'transition: width 1s ease; height: 25px; background-color: #b43bff; margin: 0; padding: 0;';
-
-        this.attributeChangedCallback();
-
-        this.parent.appendChild(this.child);
-        this.appendChild(this.parent);
-    }
-
-    static get observedAttributes() {
-        return ['value', 'initial', 'final'];
-    }
-
-    get value() {
-        return Number(this.getAttribute('value'));
-    }
-
-    set value(val) {
-        return this.setAttribute('value', val);
-    }
-
-    get initial() {
-        return Number(this.getAttribute('initial'));
-    }
-
-    set initial(val) {
-        return this.setAttribute('initial', val);
-    }
-
-    get final() {
-        return Number(this.getAttribute('final'));
-    }
-
-    set final(val) {
-        return this.setAttribute('final', val);
-    }
-
-    attributeChangedCallback () {
-        let initial = this.initial,
-            final = this.final - initial,
-            value = this.value - initial,
-            width = (100 / final) * value;
-
-        this.child.style.width = width >= 0 ? width + '%' : 0;
-    }
+(() => {
+    let elem = document.createElement('style');
+    elem.textContent = `ds-progress div {
+    background-color: #eee;
+    min-width: 100px;
+    margin-top: 5px;
+    height: 25px;
+    width: auto;
 }
 
-// Define the custom progress element
-customElements.define('ds-progress', Progress);
+ds-progress span {
+    transition: width 1s ease;
+    background-color: #b43bff;
+    display: block;
+    height: 25px;
+    padding: 0;
+    margin: 0;
+}`;
+    document.head.appendChild(elem);
+
+    class Progress extends HTMLElement {
+        constructor() {
+
+            // Always call super first in constructor
+            super();
+
+            this.parent = document.createElement('div');
+            this.child = document.createElement('span');
+        }
+
+        connectedCallback() {
+
+            this.attributeChangedCallback();
+
+            this.parent.appendChild(this.child);
+            this.appendChild(this.parent);
+        }
+
+        static get observedAttributes() {
+            return ['value', 'initial', 'final'];
+        }
+
+        get value() {
+            return Number(this.getAttribute('value'));
+        }
+
+        set value(val) {
+            return this.setAttribute('value', val);
+        }
+
+        get initial() {
+            return Number(this.getAttribute('initial'));
+        }
+
+        set initial(val) {
+            return this.setAttribute('initial', val);
+        }
+
+        get final() {
+            return Number(this.getAttribute('final'));
+        }
+
+        set final(val) {
+            return this.setAttribute('final', val);
+        }
+
+        attributeChangedCallback() {
+            let initial = this.initial,
+                final = this.final - initial,
+                value = this.value - initial,
+                width = (100 / final) * value;
+
+            this.child.style.width = width >= 0 ? width + '%' : 0;
+        }
+    }
+
+    // Define the custom progress element
+    customElements.define('ds-progress', Progress);
+})();
